c59a163f085a9def8ab04bedc28fab139cdcefb0
Thomas Wesley <tomjwesley@gmail.com>
2024-08-19 13:10:16 -0400

    Cosmetic changes.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -35,10 +35,10 @@ public class Spawner : MonoBehaviour
         Quaternion rotation = this.gameObject.transform.rotation;
         Vector3 rotatedDirection = rotation * direction;
 
-        ball.GetComponent<Throwable>().throwVector = rotatedDirection * 15000;
+        ball.GetComponent<Throwable>().throwVector = rotatedDirection * 22000;
         ball.GetComponent<Throwable>().Throw();
-        LineDrawer.GetComponent<LineRenderer>().startWidth = 0.04f;
-        LineDrawer.GetComponent<LineRenderer>().endWidth = 0.04f;
+        LineDrawer.GetComponent<LineRenderer>().startWidth = 0.08f;
+        LineDrawer.GetComponent<LineRenderer>().endWidth = 0.08f;
     }
 
 }
